cc44dfeb4351cf462bc9ac6537c5f83b79edc98a
Sumit Grover <sumitgrover108@gmail.com>
2024-07-20 15:43:37 +0530

    creating network acl for vpc using terraform

---
+++ nacl_sg/nacl.tf
@@ -0,0 +1,48 @@
+resource "aws_network_acl" "aws_nacl" {
+  vpc_id     = data.aws_vpc.GetVPC.id
+  subnet_ids = [data.aws_subnet.GetPublicSubnet.id]
+
+  #allow ingress port 22
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = data.aws_subnet.GetPublicSubnet.cidr_block
+    from_port  = 22
+    to_port    = 22
+  }
+
+  #allow ingress port 80
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = data.aws_subnet.GetPublicSubnet.cidr_block
+    from_port  = 80
+    to_port    = 80
+  }
+
+  #allow egress port 22
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = data.aws_subnet.GetPublicSubnet.cidr_block
+    from_port  = 22
+    to_port    = 22
+  }
+
+  #allow egress port 80
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = data.aws_subnet.GetPublicSubnet.cidr_block
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "aws_nacl"
+  }
+}
---
+++ nacl_sg/output.tf
@@ -0,0 +1,4 @@
+output "NACL" {
+  value       = aws_network_acl.aws_nacl.id
+  description = "NACL is created"
+}
